6d4993c826f2e81dbeeaaade9714c34d7bf96d05
Igor Floor <igormfloor@gmail.com>
2023-01-18 17:42:55 -0300

    Initial commit

---
+++ src/main/java/com/samples/resilience4jsamples/Resilience4jSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.samples.resilience4jsamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilience4jSamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Resilience4jSamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/samples/resilience4jsamples/Resilience4jSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samples.resilience4jsamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilience4jSamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
